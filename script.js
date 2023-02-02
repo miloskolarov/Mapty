0352a8e783ad253f93c904898415df8cff259665
miloskolarov <milos.v.kolarov@gmail.com>
2023-02-02 18:35:59 +0100

    Add popup description

--- script.js
+++ script.js
@@ -189,7 +189,9 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('workout.distance')
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}${workout.description}`
+      )
       .openPopup();
   }
   _renderWorkout(workout) {
