5e5c700ec7ceac16955c5fdd5e98c6dad8ddb2c0
miloskolarov <milos.v.kolarov@gmail.com>
2023-02-02 16:14:50 +0100

    Test classes

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ class Cycling extends Workout {
 
 const run1 = new Running([39, -12], 5.2, 24, 178);
 const cycling1 = new Cycling([39, -12], 27, 95, 523);
-
+console.log(run1, cycling1);
 new /* _______________________________________________________________________ */
 //Aplication
 (class App {
